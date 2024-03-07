6d611be5d0ce6b5cbf5fe2cc43bd625cc8bbc57a
0andreea <andreeamarginas@gmail.com>
2024-03-07 20:01:21 +0200

    sesiune4

--- session5/src/com/itschool/course/course5.java
+++ session5/src/com/itschool/course/course5.java
@@ -1,4 +1,27 @@
 package com.itschool.course;
 
+import java.util.Scanner;
+
 public class course5 {
+public static void main(String[] args) {
+    // & - and
+    //  && - and
+    // | - or
+    // | - or
+
+
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Insert boolean: ");
+    String a = scanner.nextLine(); ;
+
+    boolean trueVar = true;
+    boolean falseVar= true;
+    Boolean var = null;
+
+    boolean result = trueVar || falseVar || var;
+
+    System.out.println(result);
+
+    System.out.println (trueVar | falseVar | var);
+  }
 }
